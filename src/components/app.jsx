7b418748136c3f54571e4de28b5f7cbe443a6742
Eric Majerus <fake@fake.com>
2019-02-19 00:40:21 -0600

    Made App a stateless component instead of a class

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,37 +2,19 @@ import './app.less'
 import React from 'react'
 import {Desktop, Mobile} from '../responsive'
 import {connect} from 'react-redux'
-import {ConnectedMyChart} from './my-chart'
 
-export class App extends React.Component {
+export const App = () => (
+    <div className='app'>
+        <Desktop>
+            {null}
+        </Desktop>
+        <Mobile>
+            {null}
+        </Mobile>
+    </div>
+)
 
-    render() {
-        const {date, temperature} = this.props
-
-        return (
-            <div className='app'>
-                <Desktop>
-                    Observation: {date && <span>{date.toDateString()} {date.toLocaleTimeString()}</span>}
-                    Temperature: {temperature.value}{temperature.unit}
-                    <ConnectedMyChart/>
-                </Desktop>
-                <Mobile>
-                    <button>Currently</button>
-                    <button>Hourly</button>
-                    <button>Daily</button>
-                </Mobile>
-            </div>
-        )
-    }
-}
-
-const mapStateToProps = state => {
-    return {
-        date: state.currently.time,
-        temperature: state.currently.temperature || {},
-    }
-}
-
+const mapStateToProps = state => ({})
 const mapDispatchToProps = dispatch => ({})
 
 export const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App)
