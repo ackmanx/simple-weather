14255e0a76af94e026344e11400dffbdafc1e90b
Ackmanx <7490057+ackmanx@users.noreply.github.com>
2024-07-12 19:44:51 -0500

    Start over with the UX

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,7 +27,7 @@
       href="https://api.open-meteo.com/v1/forecast?latitude=45.06&longitude=-93.2052&current=dew_point_2m,temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m,wind_gusts_10m&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago&forecast_days=1"
       target="_blank"
     >
-      Current Conditions API
+      Open Meteo
     </a>
   </p>
 </footer>
