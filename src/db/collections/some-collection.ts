596f043cbdf54a4a77538c7103395374873761fd
Ackmanx <7490057+ackmanx@users.noreply.github.com>
2024-07-14 14:57:17 -0500

    Remove database

--- src/db/collections/some-collection.ts
+++
@@ -1,4 +0,0 @@
-import type { SomeDbEntity } from '$db/entity-types'
-import db from '$db/mongo'
-
-export const SomeCollection = db.collection<SomeDbEntity>('some-collection')
--- src/db/entity-types.ts
+++
@@ -1,5 +0,0 @@
-// https://patorjk.com/software/taag/#p=display&c=c&f=Delta%20Corps%20Priest%201
-
-export interface SomeDbEntity {
-  createdForTesting: string
-}
